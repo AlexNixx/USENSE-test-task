dcb9f3caf29bc037547b20803e5d5c80d9c35225
AlexNix <alexnix.mail@gmail.com>
2023-07-21 00:48:22 +0300

    implement password strength

--- src/app/component/registration/registration.component.ts
+++ src/app/component/registration/registration.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent {
+  passwordStrength: number = 0;
+
+  checkPasswordStrength(event: Event) {
+    const password = (event.target as HTMLInputElement).value;
+    this.passwordStrength = this.calculatePasswordStrength(password);
+  }
+
+  calculatePasswordStrength(password: string): number {
+    if (password.length === 0) {
+      return 0;
+    }
+
+    if (password.length < 8) {
+      return 1;
+    }
+
+    let strength = 1;
+
+    //RegEx for lower and uppercase letter
+    if (password.match(/([a-zA-Z])/)) {
+      strength += 1;
+    }
+    //RegEx for digit
+    if (password.match(/([0-9])/)) {
+      strength += 1;
+    }
+    //Regex for symbols
+    if (password.match(/([!,%,&,@,#,$,^,*,?,_,~,{,}])/)) {
+      strength += 1;
+    }
+
+    // 0 - grey
+    // 1 - red 
+    // 2 - easy 
+    // 3 - medium 
+    // 4 - strong
+    return strength
+
+  }
+
+  submit(register: any) {
+    console.log('Submit button clicked.', register);
+  }
 
 }
